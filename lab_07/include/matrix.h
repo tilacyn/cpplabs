0ffbf3fe9f751c8d92192b77aaa4f765608a1c21
tilacyn <tilacyn@yandex.ru>
2016-12-02 21:34:18 +0400

    Add files via upload

--- lab_07/include/matrix.h
+++ lab_07/include/matrix.h
@@ -1,29 +1,26 @@
-#ifndef _MATRIX_H_INCLUDED
-#define _MATRIX_H_INCLUDED
-#include <cstdio>
+#pragma once
+
 #include <cstddef>
+
 class Matrix {
-	public:
-		Matrix(const std::size_t r, const std::size_t c);
-		Matrix(const Matrix& m);
-		std::size_t get_rows() const;
-		std::size_t get_cols() const;
-		void set(const std::size_t i, const std::size_t j, const int val);
-		int get(const std::size_t i, const std::size_t j) const;
-		void print(FILE *f) const;
-		void wow_print() const;
-		Matrix operator+(const Matrix& m) const;
-		Matrix operator-(const Matrix& m) const;
-		const Matrix& operator=(const Matrix &m);
-		Matrix operator*(const Matrix& m) const;
-		Matrix& operator+=(const Matrix& m);
-		Matrix& operator-=(const Matrix& m);
-		Matrix& operator*=(const Matrix& m);
-		bool operator==(const Matrix& m) const;
-		bool operator!=(const Matrix& m) const;
-	private:
-		std::size_t _rows;
-		std::size_t _cols;
-		int **_data;
+public:
+  Matrix(std::size_t r, std::size_t c);
+  ~Matrix();
+
+  std::size_t get_rows() const;
+  std::size_t get_cols() const;
+  void set(std::size_t i, std::size_t j, int val);
+  int get(std::size_t i, std::size_t j) const;
+
+  bool operator==(const Matrix& that) const;
+  bool operator!=(const Matrix& that) const;
+
+private:
+  Matrix(const Matrix& that);
+  Matrix& operator=(const Matrix& that);
+
+private:
+  std::size_t _rows;
+  std::size_t _cols;
+  int **_data;
 };
-#endif
